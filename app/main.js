e3ffc297ff8894c8d80cae8723e1a9ed16e03633
Stuart Roth <stuartaroth@gmail.com>
2016-04-16 22:06:34 -0600

    Added prod mode

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
-System.register(['angular2/platform/browser', './app.component'], function(exports_1, context_1) {
+System.register(['angular2/platform/browser', './app.component', 'angular2/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var browser_1, app_component_1;
+    var browser_1, app_component_1, core_1;
     return {
         setters:[
             function (browser_1_1) {
@@ -9,8 +9,12 @@ System.register(['angular2/platform/browser', './app.component'], function(expor
             },
             function (app_component_1_1) {
                 app_component_1 = app_component_1_1;
+            },
+            function (core_1_1) {
+                core_1 = core_1_1;
             }],
         execute: function() {
+            core_1.enableProdMode();
             browser_1.bootstrap(app_component_1.AppComponent);
         }
     }
